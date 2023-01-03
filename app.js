57552e72ac1c514fb58fb60a894d73e4ebebca5f
myka <98901420+BMyka@users.noreply.github.com>
2023-01-03 15:27:36 +0200

    Fix calculator logic

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-
 let displayValue = "";
 let operatorClicked = false;
 let equalClicked = false;
+let afterCommaFirst = false;
+let afterCommaSecond = false;
 
 let firstValue;
 let secondValue;
 let operator;
 
 const screen = document.querySelector('.screen');
+
 function add(a, b){
     return a + b;
 }
@@ -28,119 +30,148 @@ function modulus(a, b){
 }
 
 function operate(a, operator, b){
-    if(operator == "+") return add(a, b);
+    if(operator == "+") return a + b;
     else if (operator == "-") return minus(a, b);
     else if (operator == "*") return multiplication(a, b);
     else if (operator == "/") return division(a, b);
     else if (operator == "%") return modulus(a, b);
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('click', function(e) {
-    e.stopPropagation();
-    if (square.textContent == '+/-' && typeof secondValue == 'undefined'){
-        firstValue = -firstValue;
-        displayValue = firstValue;
-        updateScreen();
-    }
-    else if (square.textContent == '+/-' && typeof secondValue !== 'undefined'){
-        secondValue = -secondValue;
-        displayValue = secondValue;
-        updateScreen();
-    }
-    
-    else if (square.textContent == 'AC'){
-        let operatorClicked = false;
-        let equalClicked = false;
-        firstValue = undefined;
-        secondValue = undefined;
-        operator = undefined;
-        displayValue = "";
-        updateScreen();
-    }
-    else if(square.classList.contains('operator') && square.textContent !== '=' && typeof firstValue !=='undefined' && typeof secondValue !== 'undefined'){
-        firstValue = operate(firstValue, operator, secondValue);
-        console.log(square.textContent);
-        secondValue = 'undefined';
-        operator = square.textContent;
-        operatorClicked = false;
-        equalClicked = true;
-        displayValue = firstValue;
-        updateScreen();
-    }
-    else if(square.classList.contains('operator') && square.textContent !== '=' && typeof firstValue !=='undefined' && typeof secondValue == 'undefined'){
-        firstValue = operate(firstValue, operator, secondValue);
-        console.log(square.textContent);
-        secondValue = 'undefined';
-        operator = square.textContent;
-        operatorClicked = false;
-        equalClicked = true;
-        displayValue = firstValue;
-        updateScreen();
-    }
-    else if(square.classList.contains('number') && equalClicked && !operatorClicked){
+function updateScreen(){
+    screen.textContent = displayValue;
+}
+
+const numbers = document.querySelectorAll('.number')
+numbers.forEach(number => number.addEventListener('click', function(e) {
+    if(equalClicked && !operatorClicked){
         equalClicked = false;
-        displayValue = square.textContent
-        updateScreen();
+        afterCommaFirst = false;
+        afterCommaSecond = false;
+        displayValue = number.textContent;
         firstValue = parseFloat(displayValue);
-    }
-    else if (square.classList.contains('number') && operatorClicked) {
-        if(typeof secondValue == 'undefined'){
-            displayValue += square.textContent;
-            secondValue = parseFloat(displayValue);
-            updateScreen();
-        }
-        else if(!(secondValue.toString()).includes('.')){
-            displayValue += square.textContent;
-            secondValue = parseFloat(displayValue);
-            updateScreen();
-        }
-    }
-    else if (square.classList.contains('number')) {
-        if(typeof firstValue == 'undefined'){
-            displayValue += square.textContent;
-            firstValue = parseFloat(displayValue);
-            updateScreen();
-        }
-        else if(!(firstValue.toString()).includes('.')){
-            displayValue += square.textContent;
-            firstValue = parseFloat(displayValue);
-            updateScreen();
-        }
-    }
-    else if (square.classList.contains('equal') && !operatorClicked && typeof secondValue == 'undefined'){
-        equalClicked = true;
-    }
-    else if (square.classList.contains('equal') && operatorClicked && typeof secondValue == 'undefined') {
-        secondValue = firstValue;
-        console.log(firstValue, operator, secondValue);
-        firstValue = operate(firstValue, operator, secondValue);
-        secondValue = undefined;
-        operatorClicked = false;
-        equalClicked = true;
-        displayValue = firstValue;
-        updateScreen();
-        console.log("test");
-    }
-    else if (square.classList.contains('equal') && operatorClicked && typeof secondValue !== 'undefined') {
-        firstValue = operate(firstValue, operator, secondValue);
-        secondValue = undefined;
-        operatorClicked = false;
-        equalClicked = true;
-        displayValue = firstValue;
         updateScreen();
     }
-    else if (square.classList.contains('operator')){
+    else if(operatorClicked && typeof secondValue == 'undefined'){
+        displayValue = number.textContent;
+        secondValue = parseFloat(displayValue);
+        updateScreen();
+    }
+    else if(operatorClicked){
+        displayValue += number.textContent;
+        secondValue = parseFloat(displayValue);
+        updateScreen();
+    }
+    else if(!operatorClicked){
+        displayValue += number.textContent;
+        firstValue = parseFloat(displayValue);
+        updateScreen();
+    }
+}))
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach(op => op.addEventListener('click', function(e){
+    if(!operatorClicked){
+        operator = op.textContent;
         operatorClicked = true;
-        operator = square.textContent;
-        displayValue = "";
+    }
+    else if(operatorClicked && typeof secondValue !== 'undefined'){
+        firstValue = operate(firstValue, operator, secondValue);
+        displayValue = parseFloat(firstValue.toFixed(4));
+        secondValue = undefined;
+        afterCommaSecond = false;
+        operator = op.textContent;
+        operatorClicked = true;
+        equalClicked = true;
+        updateScreen();
+
+    }
+    else if(operatorClicked){
+        operator = undefined;
+        operatorClicked = false;
     }
 }))
 
 
+const equal = document.querySelector('.equal');
+equal.addEventListener('click', function(e) {
+    if(operatorClicked && typeof secondValue == 'undefined'){
+        firstValue = operate(firstValue, operator, firstValue);
+        displayValue = parseFloat(firstValue.toFixed(4));
+        secondValue = undefined;
+        afterCommaSecond = false;
+        operator = undefined;
+        operatorClicked = false;
+        equalClicked = true;
+        updateScreen();
+    }
+    else if(operatorClicked){
+        firstValue = operate(firstValue, operator, secondValue);
+        displayValue = parseFloat(firstValue.toFixed(4));
+        secondValue = undefined;
+        afterCommaSecond = false;
+        operator = undefined;
+        operatorClicked = false;
+        equalClicked = true;
+        updateScreen();
+    }
+})
 
 
+const comma = document.querySelector('.comma');
+comma.addEventListener('click', function(e) {
+    if(!afterCommaFirst && !operatorClicked){
+        displayValue += e.target.textContent;
+        updateScreen();
+        afterCommaFirst= true;
+    }
+    if(!afterCommaSecond && operatorClicked){
+        displayValue += e.target.textContent;
+        updateScreen();
+        afterCommaSecond = true;
+    }
+})
 
-function updateScreen(){
-    screen.textContent = displayValue;
+const clearEverything = document.querySelector('.AC');
+clearEverything.addEventListener('click', function(e){
+    reset();
+})
+
+function reset(){
+    displayValue = "";
+    operatorClicked = false;
+    equalClicked = false;
+    afterCommaFirst = false;
+    afterCommaSecond = false;
+    firstValue = undefined;
+    secondValue = undefined;
+    operator = undefined;
+    updateScreen();
 }
+
+const changeSign = document.querySelector('.sign');
+changeSign.addEventListener('click', function(e){
+    if(operatorClicked && Number.isInteger(secondValue)){
+        afterCommaSecond = false;
+        secondValue = -secondValue;
+        displayValue = secondValue;
+        updateScreen();
+    }
+    else if(operatorClicked && !Number.isInteger(secondValue)){
+        secondValue = -secondValue;
+        displayValue = secondValue;
+        updateScreen();
+    }
+    else if(!operatorClicked && Number.isInteger(firstValue)){
+        afterCommaFirst = false;
+        equalClicked = false;
+        firstValue = -firstValue;
+        displayValue = firstValue;
+        updateScreen();
+    }
+    else if(!operatorClicked && !Number.isInteger(firstValue)){
+        equalClicked = false;
+        firstValue = -firstValue;
+        displayValue = firstValue;
+        updateScreen();
+    }
+})
